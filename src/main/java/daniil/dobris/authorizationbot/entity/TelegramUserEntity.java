d36f22a648146ca30e865585eb8288de7a02dc9e
dandobri <dobris04@mail.ru>
2025-05-28 23:23:14 +0300

    added database entity

---
+++ src/main/java/daniil/dobris/authorizationbot/entity/TelegramUserEntity.java
@@ -0,0 +1,20 @@
+package daniil.dobris.authorizationbot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class TelegramUserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String photoUrl;
+}
